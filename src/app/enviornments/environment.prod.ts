094cc03bfbec1548141f68a336e17d033b401602
Saisandeep2000 <saisandeep3585@gmail.com>
2024-08-17 12:37:19 +0530

    changed baseurl

--- src/app/enviornments/environment.prod.ts
+++ src/app/enviornments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    url:"http://localhost:5001",
+    // url:"http://localhost:5001",
+    // url:"http://172.105.56.131:5001",
+    url:"https://websitedata.ctfc.in",
 }
--- src/app/enviornments/environment.ts
+++ src/app/enviornments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    url:"http://localhost:5001",
+    // url:"http://localhost:5001",
+    // url:"http://172.105.56.131:5001",
+    url:"https://websitedata.ctfc.in",
 }
